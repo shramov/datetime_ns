2e678eba7c6651f00669d3ca25cb853226ecc1d3
Pavel Shramov <psha@nguni.psha.org.ru>
2020-09-08 09:28:04 +0300

    Added simple setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# vim: sts=4 sw=4 et
+
+from distutils.core import setup
+
+setup(name='datetime_ns',
+      version='0.1',
+      description='Datetime module with nanosecond precision',
+      author='Pavel Shramov',
+      author_email='shramov@mexmat.net',
+      url='https://github.com/shramov/datetime_ns/',
+      packages=['datetime_ns'],
+     )
